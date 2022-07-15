052fb63afb8a0648c6134fd1b49e465464bec625
dkjb634 <dmitry.kazantsev@jetbrains.com>
2022-07-15 12:56:38 +0300

    www

---
+++ cs_git2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cs_git2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
